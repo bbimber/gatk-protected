75342f24b8fe8fcca7fb5a52e93497f70d856cea
melissayan <melyan@pdx.edu>
2017-05-26 09:11:24 -0700

    html table working

--- public/gatk-tools-public/src/main/java/org/broadinstitute/gatk/tools/walkers/variantqc/TableReportDescriptor.java
+++ public/gatk-tools-public/src/main/java/org/broadinstitute/gatk/tools/walkers/variantqc/TableReportDescriptor.java
@@ -1,22 +1,36 @@
 package org.broadinstitute.gatk.tools.walkers.variantqc;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.broadinstitute.gatk.utils.report.GATKReportColumn;
+import org.broadinstitute.gatk.utils.report.GATKReportDataType;
 
-import java.util.Map;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
 
 /**
  * Created by bimber on 5/22/2017.
  */
 public class TableReportDescriptor extends ReportDescriptor {
+    Gson gson = new GsonBuilder().create();
+
     public TableReportDescriptor(String label, SectionJsonDescriptor.PlotType plotType, String evaluatorModuleName) {
         super(label, plotType, evaluatorModuleName);
     }
 
     public static TableReportDescriptor getCountVariantsTable() {
-        return new TableReportDescriptor("Variant Summary", SectionJsonDescriptor.PlotType.data_table, "CountVariants");
+        TableReportDescriptor ret = new TableReportDescriptor("Variant Summary", SectionJsonDescriptor.PlotType.data_table, "CountVariants");
+
+        JsonObject myColJson = new JsonObject();
+        myColJson.addProperty("dmin", 0);
+        myColJson.addProperty("dmax", 1.0);
+        ret.addColumnInfo("myColumn", myColJson);
+
+        return ret;
     }
 
     @Override
@@ -24,34 +38,75 @@ public class TableReportDescriptor extends ReportDescriptor {
         JsonObject ret = new JsonObject();
         ret.addProperty("label", label);
 
-        JsonObject data = new JsonObject();
-        ret.add("data", data);
+        JsonObject dataObj = new JsonObject();
+        ret.add("data", dataObj);
 
-        data.addProperty("plot_type", plotType.name());
-        data.add("samples", getSampleNames());
-        data.add("datasets", new JsonArray());
+        dataObj.addProperty("plot_type", plotType.name());
 
-        data.add("columns", new JsonArray());
+        dataObj.add("samples", getSampleNames());//Ordering of sample names must correspond with dataset order
+
+        JsonArray datasetsJson = new JsonArray();
+        for (Object rowId : table.getRowIDs()) {
+            JsonArray rowJson = new JsonArray();
+            List<Object> rowList = new ArrayList<>();
+            for (GATKReportColumn col : table.getColumnInfo()) {
+                rowList.add(table.get(rowId, col.getColumnName()));
+                rowJson = gson.toJsonTree(rowList).getAsJsonArray();
+            }
+            datasetsJson.add(rowJson);
+        }
+        dataObj.add("datasets", datasetsJson);
+
+        dataObj.add("columns", new JsonArray());
         for (GATKReportColumn col : table.getColumnInfo()) {
             JsonObject colJson = new JsonObject();
             colJson.addProperty("name", col.getColumnName());
             colJson.addProperty("label", col.getColumnName());
 
-            //we will probably need a way to provide information beyond just what is in the GATKReportTable itself
-            if (columnInfoMap.containsKey(col.getColumnName())) {
-                for (Map.Entry<String, JsonElement> entry : columnInfoMap.get(col.getColumnName()).entrySet()) {
-                    colJson.add(entry.getKey(), entry.getValue());
+            Object dataType = col.getDataType();
+            if (dataType.toString().equals(GATKReportDataType.Decimal.toString())){
+                colJson.addProperty("formatString", "0.00");
+                List<Double> rowValuesList = new ArrayList<>();
+                for (Object rowId : table.getRowIDs()) {
+                    rowValuesList.add(NumberUtils.createNumber(table.get(rowId, col.getColumnName()).toString()).doubleValue());
                 }
+                Double min = Collections.min(rowValuesList) - Collections.min(rowValuesList)*0.1;
+                Double max = Collections.max(rowValuesList) + Collections.max(rowValuesList)*0.1;
+                colJson.addProperty("dmin", min);
+                if (max == 0){
+                    colJson.addProperty("dmax", max+1);
+                } else {
+                    colJson.addProperty("dmax", max);
+                }
+            } else if (dataType.toString().equals(GATKReportDataType.Integer.toString())){
+                colJson.addProperty("formatString", "");
+                List<Integer> rowValuesList = new ArrayList<>();
+                for (Object rowId : table.getRowIDs()) {
+                    rowValuesList.add(NumberUtils.createNumber(table.get(rowId, col.getColumnName()).toString()).intValue());
+                }
+                Double min = Collections.min(rowValuesList) - Collections.min(rowValuesList)*0.1;
+                Double max = Collections.max(rowValuesList) + Collections.max(rowValuesList)*0.1;
+                colJson.addProperty("dmin", min);
+                if (max == 0){
+                    colJson.addProperty("dmax", max+1);
+                } else {
+                    colJson.addProperty("dmax", max);
+                }
+            } else {
+                colJson.addProperty("formatString", "");
+                colJson.addProperty("dmin", "");
+                colJson.addProperty("dmax", "");
             }
 
-
-            if (!colJson.has("dmin")) {
-                //TODO: infer based on data.  perhaps find the min/max and take +/- 10%?
-            }
-
-            data.getAsJsonArray("columns").add(colJson);
+//            if (columnInfoMap.containsKey(col.getColumnName())){
+//                for (Map.Entry<String, JsonElement> e : columnInfoMap.get(col.getColumnName()).entrySet()){
+//                    colJson.add(e.getKey(), e.getValue());
+//                }
+//            }
+            dataObj.getAsJsonArray("columns").add(colJson);
         }
 
         return ret;
+
     }
 }
